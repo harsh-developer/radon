039ae1c9a79a0f49f64b791b90f760e3544129f3
harsh-developer <harshpratap1444@gmail.com>
2022-06-09 23:53:39 +0530

    only 5th problem remaining

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId
 
 const bookSchema = new mongoose.Schema( {
-    name: String,
+    bookName: String,
     author_id: {
         type: ObjectId,
         ref: "Author"
     },
     price: Number,
-    ratings: Number
-
+    ratings: Number,
+    publisher: {
+        type: ObjectId,
+        ref: "Publisher"
+    }
 
 }, { timestamps: true });
 
